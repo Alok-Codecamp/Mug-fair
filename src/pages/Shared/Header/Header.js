661a4eaf5258f4638d879434361c4c3e9df48e13
Alok Kumar <alok61.bd@gmail.com>
2021-11-15 06:04:29 +0600

    navbar edited

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark" style={{background:'#51050F'}}>
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="#">Mug Fair</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
